6fc65203580dedabdbd4e2e3afffab09e04a65c1
NFM <jwj0831@notforme.kr>
2015-02-25 16:26:06 +0900

    코드 리팩토링

--- csn-middleware/src/main/java/org/csn/component/threads/CentralizedDataDistributor.java
+++ csn-middleware/src/main/java/org/csn/component/threads/CentralizedDataDistributor.java
@@ -2,8 +2,11 @@ package org.csn.component.threads;
 
 import org.csn.data.NetworkMappingMap;
 import org.csn.data.SensorData;
+import org.csn.util.LogPrinter;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,137 +15,146 @@ import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 
 public class CentralizedDataDistributor extends Thread implements MqttCallback {
-    Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(CentralizedDataDistributor.class.getClass());
 
-    MqttClient myClient;
-    MqttConnectOptions connOpt;
+	MqttClient myClient;
+	MqttConnectOptions connOpt;
 
-    protected BlockingQueue<SensorData> queue;
-    static final String CLIENT_ID = "Publisher-Node";
-    static final String BROKER_URL = "tcp://localhost:1883";
+	protected BlockingQueue<SensorData> queue;
+	static final String CLIENT_ID = "Publisher-Node";
+	static final String BROKER_URL = "tcp://localhost:1883";
 
-    private volatile boolean stopped = false;
+	private volatile boolean stopped = false;
 
-    public CentralizedDataDistributor(String name, BlockingQueue<SensorData> queue) {
-        super(name);
-        this.queue = queue;
-    }
+	public CentralizedDataDistributor(String name,
+			BlockingQueue<SensorData> queue) {
+		super(name);
+		this.queue = queue;
+	}
 
-    private void before() throws Exception {
-        logger.info("Connecting to Messaging Queue");
+	private void openMQTTConn() throws Exception {
+		LOGGER.info("Connecting to Messaging Queue");
 
-        // Setup MQTT Client
-        connOpt = new MqttConnectOptions();
-        connOpt.setCleanSession(true);
-        connOpt.setKeepAliveInterval(30);
+		// Setup MQTT Client
+		connOpt = new MqttConnectOptions();
+		connOpt.setCleanSession(true);
+		connOpt.setKeepAliveInterval(30);
 
-        // Connect to Broker
-        try {
-            myClient = new MqttClient(BROKER_URL, CLIENT_ID);
-            myClient.setCallback(this);
-            myClient.connect(connOpt);
-        } catch (MqttException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
+		// Connect to Broker
+		try {
+			myClient = new MqttClient(BROKER_URL, CLIENT_ID);
+			myClient.setCallback(this);
+			myClient.connect(connOpt);
+		} catch (MqttException e) {
+			LogPrinter.printErrorLog(LOGGER, e.getClass().toString(),
+					e.getMessage());
+			System.exit(-1);
+		}
 
-        logger.info("Connected to " + BROKER_URL);
-    }
+		LOGGER.info("Connected to " + BROKER_URL);
+	}
 
-    public void after() throws Exception {
-        logger.info("Closing Connection");
-//        if (connection != null) {
-//            connection.close();
-//        }
+	public void closeMQTTconn() throws Exception {
+		LOGGER.info("Closing Connection");
 
-        // disconnect
-        try {
-            myClient.disconnect();
-            System.out.println("Close Connection");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		// disconnect
+		try {
+			myClient.disconnect();
+			System.out.println("Close Connection");
+		} catch (Exception e) {
+			LogPrinter.printErrorLog(LOGGER, e.getClass().toString(),
+					e.getMessage());
+		}
+	}
 
-    public void connectionLost(Throwable t) {
-        System.out.println("Connection lost!");
-    }
+	public void connectionLost(Throwable t) {
+		System.out.println("Connection lost!");
+	}
 
-    public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
-    }
+	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+	}
 
-    public void deliveryComplete(IMqttDeliveryToken arg0) {
-    }
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+	}
 
-    @Override
-    public void run() {
-        try {
-            before();
-            while( !isStopped() ) {
-                publishData();
-            }
-            after();
-            logger.info("Thread will be stopped");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void run() {
+		try {
+			openMQTTConn();
+			while (!isStopped()) {
+				publishData();
+			}
+			closeMQTTconn();
+			LOGGER.info("Thread will be stopped");
+		} catch (Exception e) {
+			LogPrinter.printErrorLog(LOGGER, e.getClass().toString(),
+					e.getMessage());
+		}
+	}
 
-    private void publishData() {
-        SensorData sensorData = null;
+	private void publishData() {
 
-        if( queue.peek() != null ) {
-            logger.info("Pop Sensor Data from Central Queue ...");
-            sensorData = queue.poll();
+		if (queue.peek() != null) {
+			LOGGER.info("Pop Sensor Data from Central Queue ...");
+			SensorData sensorData = queue.poll();
 
-            Set<String> topicPaths = NetworkMappingMap.getMappingMap().get(sensorData.getId());
-            if(topicPaths != null) {
-            	logger.info("Data ID: {} is registered in the CSN", sensorData.getId());
-                for(String topicPath : topicPaths) {
-                    logger.info("Current Sensor Data is a member of {}", topicPath);
+			Set<String> topicPaths = NetworkMappingMap.getMappingMap().get(
+					sensorData.getId());
 
-                    String mqttTopicName = topicPath.replace('.', '/');
-                    // Setup topic
-                    MqttTopic topic = myClient.getTopic(mqttTopicName);
-                    ObjectMapper jsonMapper = new ObjectMapper();
+			if (topicPaths != null) {
+				distributeSnsrData(sensorData, topicPaths);
+			} else {
+				LOGGER.info("Data ID: {} is not registered in the CSN",
+						sensorData.getId());
+			}
+		}
+	}
 
-                    String jsonStr = null;
-                    try {
-                        jsonStr = jsonMapper.writeValueAsString(sensorData);
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("RePublished MSG: " + jsonStr);
+	
+	private void distributeSnsrData(SensorData sensorData,
+			Set<String> topicPaths) {
+		LOGGER.info("Data ID: {} is registered in the CSN", sensorData.getId());
+		for (String topicPath : topicPaths) {
+			LOGGER.info("Current Sensor Data is a member of {}", topicPath);
 
-                    int pubQoS = 0;
-                    MqttMessage message = new MqttMessage(jsonStr.getBytes());
-                    message.setQos(pubQoS);
-                    message.setRetained(false);
-                    MqttDeliveryToken token = null;
-                    try {
-                        // publish message to broker
-                        token = topic.publish(message);
-                        // Wait until the message has been delivered to the broker
-                        token.waitForCompletion();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            else {
-            	logger.info("Data ID: {} is not registered in the CSN", sensorData.getId());
-            }
-        }
-    }
+			String mqttTopicName = topicPath.replace('.', '/');
+			// Setup topic
+			MqttTopic topic = myClient.getTopic(mqttTopicName);
+			ObjectMapper jsonMapper = new ObjectMapper();
 
-    public boolean isStopped() {
-        return stopped;
-    }
+			String jsonStr = null;
+			try {
+				jsonStr = jsonMapper.writeValueAsString(sensorData);
+			} catch (JsonProcessingException e) {
+				LogPrinter.printErrorLog(LOGGER, e.getClass().toString(),
+						e.getMessage());
+			}
+			LOGGER.trace("RePublished MSG: {}", jsonStr);
 
-    public void setStopped(boolean stopped) {
-        this.stopped = stopped;
-    }
+			int pubQoS = 0;
+			MqttMessage message = new MqttMessage(jsonStr.getBytes());
+			message.setQos(pubQoS);
+			message.setRetained(false);
+			MqttDeliveryToken token = null;
+			try {
+				// publish message to broker
+				token = topic.publish(message);
+				// Wait until the message has been delivered to the
+				// broker
+				token.waitForCompletion();
+			} catch (Exception e) {
+				LogPrinter.printErrorLog(LOGGER, e.getClass().toString(),
+						e.getMessage());
+			}
+		}
+	}
 
+	public boolean isStopped() {
+		return stopped;
+	}
 
-
+	public void setStopped(boolean stopped) {
+		this.stopped = stopped;
+	}
 }
