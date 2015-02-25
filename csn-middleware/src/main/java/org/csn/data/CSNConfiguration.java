54d1af3d942e899da92f0348c58bd6aeed5a8619
NFM <jwj0831@notforme.kr>
2015-02-25 18:02:46 +0900

    toString 메서드 구아바로 교체

--- csn-middleware/src/main/java/org/csn/data/CSNConfiguration.java
+++ csn-middleware/src/main/java/org/csn/data/CSNConfiguration.java
@@ -2,85 +2,84 @@ package org.csn.data;
 
 import org.csn.util.TimeGenerator;
 
+import com.google.common.base.MoreObjects;
+
 public class CSNConfiguration {
-    private String csnName;
-    private String creationTime;
-    private String adminName;
-    private String adminEmail;
-    private boolean persistOption;
+	private String csnName;
+	private String creationTime;
+	private String adminName;
+	private String adminEmail;
+	private boolean persistOption;
 
-    public CSNConfiguration() {
-        this.csnName = "";
-        this.creationTime = "";
-        this.adminName = "";
-        this.adminEmail = "";
-        this.persistOption = true;
-    }
+	public CSNConfiguration() {
+		this.csnName = "";
+		this.creationTime = "";
+		this.adminName = "";
+		this.adminEmail = "";
+		this.persistOption = true;
+	}
 
-    public CSNConfiguration(String csnName) {
-        this.csnName = csnName;
-        this.creationTime = TimeGenerator.getCurrentTimestamp();
-        this.adminName = "";
-        this.adminEmail = "";
-        this.persistOption = true;
-    }
+	public CSNConfiguration(String csnName) {
+		this.csnName = csnName;
+		this.creationTime = TimeGenerator.getCurrentTimestamp();
+		this.adminName = "";
+		this.adminEmail = "";
+		this.persistOption = true;
+	}
 
-    public CSNConfiguration(String csnName, String adminName, String adminEmail) {
-        this.csnName = csnName;
-        this.creationTime = TimeGenerator.getCurrentTimestamp();
-        this.adminName = adminName;
-        this.adminEmail = adminEmail;
-        this.persistOption = true;
-    }
+	public CSNConfiguration(String csnName, String adminName, String adminEmail) {
+		this.csnName = csnName;
+		this.creationTime = TimeGenerator.getCurrentTimestamp();
+		this.adminName = adminName;
+		this.adminEmail = adminEmail;
+		this.persistOption = true;
+	}
 
-    public String getCsnName() {
-        return csnName;
-    }
+	public String getCsnName() {
+		return csnName;
+	}
 
-    public void setCsnName(String csnName) {
-        this.csnName = csnName;
-    }
+	public void setCsnName(String csnName) {
+		this.csnName = csnName;
+	}
 
-    public String getCreationTime() {
-        return creationTime;
-    }
+	public String getCreationTime() {
+		return creationTime;
+	}
 
-    public void setCreationTime(String creationTime) {
-        this.creationTime = creationTime;
-    }
+	public void setCreationTime(String creationTime) {
+		this.creationTime = creationTime;
+	}
 
-    public String getAdminName() {
-        return adminName;
-    }
+	public String getAdminName() {
+		return adminName;
+	}
 
-    public void setAdminName(String adminName) {
-        this.adminName = adminName;
-    }
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
 
-    public String getAdminEmail() {
-        return adminEmail;
-    }
+	public String getAdminEmail() {
+		return adminEmail;
+	}
 
-    public void setAdminEmail(String adminEmail) {
-        this.adminEmail = adminEmail;
-    }
+	public void setAdminEmail(String adminEmail) {
+		this.adminEmail = adminEmail;
+	}
 
-    public boolean isPersistOption() {
-        return persistOption;
-    }
+	public boolean isPersistOption() {
+		return persistOption;
+	}
 
-    public void setPersistOption(boolean persistOption) {
-        this.persistOption = persistOption;
-    }
+	public void setPersistOption(boolean persistOption) {
+		this.persistOption = persistOption;
+	}
 
-    @Override
-    public String toString() {
-        return "CSNConfiguration{" +
-                "csnName='" + csnName + '\'' +
-                ", creationTime='" + creationTime + '\'' +
-                ", adminName='" + adminName + '\'' +
-                ", adminEmail='" + adminEmail + '\'' +
-                ", persistOption=" + persistOption +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(getClass()).add("csnName", csnName)
+				.add("creationTime", creationTime).add("adminName", adminName)
+				.add("adminEmail", adminEmail)
+				.add("persistOption", persistOption).toString();
+	}
 }
